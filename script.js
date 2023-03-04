68f7b5b1bf03cbd1ae058b9f968e06ebbb1e8ea9
Julia Georgiew <java2011@me.com>
2023-03-04 21:41:12 +0100

    added dumb PC-Player

--- script.js
+++ script.js
@@ -1,116 +1,167 @@
 let fields = [];
-
+let counter = 0;
+let amountCircles = 0;
 let currentShape = 'cross';
 let gameOver = false;
+let winner = '';
 
 
-function fillShape(id){
+function fillShape(id) {
+    if (!fields[id] && !gameOver) {
 
-    if(!fields[id] && !gameOver){
-    if(currentShape == 'cross'){
         currentShape = 'circle';
+        fields[id] = currentShape;
+        draw();
+        checkForWin();
         document.getElementById('player1').classList.add('playerInactive');
         document.getElementById('player2').classList.remove('playerInactive');
-      
-    } else {
-        currentShape = 'cross';
-        document.getElementById('player1').classList.remove('playerInactive');
-        document.getElementById('player2').classList.add('playerInactive');
+
+        if (!gameOver) {
+            setTimeout(function () {
+                pcTurn();
+            }, 1000);
+        }
+        console.log(fields);
+
     }
-    fields[id] = currentShape;
-    console.log(fields);
+}
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function pcTurn() {
+    let random = 0;
+    currentShape = 'cross';
+    random = getRandomInt(0, 8);
+    if (!fields[random] && counter < 9)
+        fields[random] = currentShape;
+    else pcTurn();
     draw();
     checkForWin();
-}
+    document.getElementById('player1').classList.remove('playerInactive');
+    document.getElementById('player2').classList.add('playerInactive');
 }
 
 
-function draw(){
-    for (let i=0; i<fields.length; i++){
-if(fields[i] == 'circle'){
-    document.getElementById(`circle-${i}`).classList.remove('d-none');
-}
-if(fields[i] == 'cross'){
-    document.getElementById(`cross-${i}`).classList.remove('d-none');
-}
+function draw() {
+    for (let i = 0; i < fields.length; i++) {
+        if (fields[i] == 'circle') {
+            document.getElementById(`circle-${i}`).classList.remove('d-none');
+        }
+        if (fields[i] == 'cross') {
+            document.getElementById(`cross-${i}`).classList.remove('d-none');
+        }
     }
 }
 
-function checkForWin(){
+
+
+
+function checkForWin() {
     let winner;
-    if(fields[0] == fields[1] && fields[1] == fields[2] && fields[0]){
+    if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
         winner = fields[0]
         document.getElementById('win1').style.transform = 'scale(1)';
     }
-    if(fields[3] == fields[4] && fields[4] == fields[5] && fields[3]){
+    if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
         winner = fields[3]
         document.getElementById('win2').style.transform = 'scale(1)';
     }
-    if(fields[6] == fields[7] && fields[7] == fields[8] && fields[6]){
+    if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
         winner = fields[6]
         document.getElementById('win3').style.transform = 'scale(1)';
     }
-    if(fields[0] == fields[3] && fields[3] == fields[6]&& fields[0]){
+    if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
         winner = fields[0]
         document.getElementById('win4').style.transform = 'rotate(90deg) scaleX(1)';
     }
-    if(fields[1] == fields[4] && fields[4] == fields[7]&& fields[1]){
+    if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
         winner = fields[1]
         document.getElementById('win5').style.transform = 'rotate(90deg) scaleX(1)';
     }
-    if(fields[2] == fields[5] && fields[5] == fields[8]&& fields[2]){
+    if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
         winner = fields[2]
         document.getElementById('win6').style.transform = 'rotate(90deg) scaleX(1)';
     }
-    if(fields[0] == fields[4] && fields[4] == fields[8]&& fields[0]){
+    if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
         winner = fields[0]
         document.getElementById('win7').style.transform = 'rotate(45deg) scaleX(1.2)';
     }
-    if(fields[2] == fields[4] && fields[4] == fields[6]&& fields[2]){
+    if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
         winner = fields[2]
         document.getElementById('win8').style.transform = 'rotate(-45deg) scaleX(1.2)';
     }
-console.log(winner);
-    if (winner){
+    console.log(winner);
+    checkForCircle();
+    if (winner) {
         console.log('gewonnen:', winner);
         gameOver = true;
-        setTimeout(function(){
+        nameOfWinner(winner);
+        setTimeout(function () {
             document.getElementById('gameOver').classList.remove('d-none');
-            document.getElementById('button').classList.remove('d-none'); 
-            
+            document.getElementById('button').classList.remove('d-none');
+            document.getElementById('whoWon').innerHTML = `${nameOfWinner(winner)} won!`;
+            document.getElementById('whoWon').classList.remove('d-none');
         }, 1000);
-                }
+        return false;
     }
-   
-function restart(){
+
+    if (amountCircles == 5) {
+        showUndecided();
+    }
+}
+
+
+
+function restart() {
     gameOver = false;
     document.getElementById('gameOver').classList.add('d-none');
-    document.getElementById('button').classList.add('d-none'); 
+    document.getElementById('button').classList.add('d-none');
+    document.getElementById('whoWon').classList.add('d-none');
     fields = [];
     currentShape = 'cross';
 
-   for (let i=0; i<9; i++){
-                   document.getElementById(`circle-${i}`).classList.add('d-none');
-                   document.getElementById(`cross-${i}`).classList.add('d-none');
-                  
-                  }
+    for (let i = 0; i < 9; i++) {
+        document.getElementById(`circle-${i}`).classList.add('d-none');
+        document.getElementById(`cross-${i}`).classList.add('d-none');
+    }
 
-                  for (let i=1; i<9; i++){
-                    document.getElementById(`win${i}`).style.transform = 'scale(0)';
-                  }
-
-
-
+    for (let i = 1; i < 9; i++) {
+        document.getElementById(`win${i}`).style.transform = 'scale(0)';
+    }
 }
 
-function changePlayer(activePlayer, inactivePlayer){
-    document.getElementById(inactivePlayer).classList.add('playerInactive');
-    document.getElementById(activePlayer).classList.remove('playerInactive');
+
+function nameOfWinner(winner) {
+    if (winner == 'circle') {
+        winner = 'You';
+    } else {
+        winner = 'DumbPC';
+    }
+    return winner;
 }
 
-// function randomIntFromInterval() { // min and max included 
-//     return Math.floor(Math.random() * (8 - 0 + 1) + 0)
-//   }
-  
-//   const rndInt = randomIntFromInterval()
-//   console.log(rndInt)
+
+function checkForCircle() {
+    amountCircles = 0;
+    for (let i = 0; i < fields.length; i++) {
+        let element = fields[i];
+        if (element == 'circle') {
+            amountCircles++
+        }
+    }
+}
+
+
+function showUndecided() {
+    gameOver = true;
+    setTimeout(function () {
+        document.getElementById('gameOver').classList.remove('d-none');
+        document.getElementById('button').classList.remove('d-none');
+        document.getElementById('whoWon').innerHTML = `nobody won!`;
+        document.getElementById('whoWon').classList.remove('d-none');
+
+    }, 1000);
+}
